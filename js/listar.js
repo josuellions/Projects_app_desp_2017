9cba9dc78e576d919fdebd4cbe96738f3a4dd9f7
josuellions <josuel_lions@hotmail.com>
2017-09-30 12:44:59 -0300

    ver_alinhamento_elementos_tela

--- js/listar.js
+++ js/listar.js
@@ -1,31 +1,35 @@
-(function($){
-	$("#btnEnter").click(function(){
-	var settings = {
-		   "async": true,
-                                "crossDomain": true,
-	  "url": "http://21.21.21.5:3000/listar/despesa",
-	  "method": "GET",
-	  "headers": {
-	    "content-type": "application/x-www-form-urlencoded",
-	    "cache-control": "no-cache",
-	    "postman-token": "21a92a35-d6fb-2b94-6e94-9033e30ffc27"
-	  }
-	}
+// (function($){
+// 	$("#btnEnter").click(function(){
+// 	var settings = {
+// 		   "async": true,
+//                                 "crossDomain": true,
+// 	  "url": "http://21.21.21.5:3000/listar/despesa",
+// 	  "method": "GET",
+// 	  "headers": {
+// 	    "content-type": "application/x-www-form-urlencoded",
+// 	    "cache-control": "no-cache",
+// 	    "postman-token": "21a92a35-d6fb-2b94-6e94-9033e30ffc27"
+// 	  }
+// 	}
 
-	$.ajax( settings).done(function(response){
-		var exib =response;
+// 	$.ajax( settings).done(function(response){
+// 		var exib =response;
 
-		$.each(exib, function(despesa_ent,valor_ent){
-			if(exib.length) {
-				$('#tbList > tbody').append(
-					'<tr>' + '<td>' + this.despesa_ent + 
-					'</td>' + '<td>' + "R$ " + this.valor_ent + 
-                        			'</td>' + '</tr>'
-                        			);
-			}
-		});
-	});
+// 		$.each(exib, function(despesa_ent,valor_ent){
+// 			if(exib.length) {
+// 				$('#tbList > tbody').append(
+// 					'<tr>' + '<td>' + this.despesa_ent + 
+// 					'</td>' + '<td>' + "R$ " + this.valor_ent + 
+//                         			'</td>' + '</tr>'
+//                         			);
+// 			}
+// 		});
+// 	});
 
+// });
+
+// })(jQuery);
+
+$("#btnEnter").click( function( ) {
+	alert("Em Desenvolvimento!!!")
 });
-
-})(jQuery);
--- js/listar_entrada.js
+++ js/listar_entrada.js
@@ -1,31 +1,31 @@
-(function($){
-	$("#btnEnter").click(function(){
-		var settings = {
-		   "async": true,
-                            "crossDomain": true,
-		  "url": "http://21.21.21.5:3000/listar_cx/despesa",
-		  "method": "GET",
-		  "headers": {
-		    "content-type": "application/x-www-form-urlencoded",
-		    "cache-control": "no-cache",
-		    "postman-token": "6686b25d-5999-6100-4a2a-96fdc3ee5c9f"
-		  }
-		}
+// (function($){
+// 	$("#btnEnter").click(function(){
+// 		var settings = {
+// 		   "async": true,
+//                             "crossDomain": true,
+// 		  "url": "http://21.21.21.5:3000/listar_cx/despesa",
+// 		  "method": "GET",
+// 		  "headers": {
+// 		    "content-type": "application/x-www-form-urlencoded",
+// 		    "cache-control": "no-cache",
+// 		    "postman-token": "6686b25d-5999-6100-4a2a-96fdc3ee5c9f"
+// 		  }
+// 		}
 
-	$.ajax( settings).done(function(response){
-		var exib =response;
+// 	$.ajax( settings).done(function(response){
+// 		var exib =response;
 
-		$.each(exib, function(descricao_cx,valor_cax){
-			if(exib.length) {
-				$('#tbList_cx > tbody').append(
-					'<tr>' + '<td>' + this.descricao_cx + 
-					'</td>' + '<td>' + "R$ " + this.valor_cx + 
-                        			'</td>' + '</tr>'
-                        			);
-			}
-		});
-	});
+// 		$.each(exib, function(descricao_cx,valor_cax){
+// 			if(exib.length) {
+// 				$('#tbList_cx > tbody').append(
+// 					'<tr>' + '<td>' + this.descricao_cx + 
+// 					'</td>' + '<td>' + "R$ " + this.valor_cx + 
+//                         			'</td>' + '</tr>'
+//                         			);
+// 			}
+// 		});
+// 	});
 
-});
+// });
 
-})(jQuery);
+// })(jQuery);
